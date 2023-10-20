5d07cf7749e90b635e3a7ad9be3a8325faa9c14e
DavvidU <dawidugniewski@gmail.com>
2023-10-20 23:59:14 +0200

    Dodaj pliki projektów.

---
+++ Interaktywny harmonogram/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.SetCursorPosition(13, 13);
+Console.Write("Witam Studenta!");
+Console.ReadKey();
+
+Console.SetCursorPosition(5, 5);
+Console.ForegroundColor = ConsoleColor.Blue;
+Console.Write("╔ ╗ ╝ ╚");
+
+Console.SetCursorPosition(25, 7);
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.BackgroundColor = ConsoleColor.DarkBlue;
+Console.Write("ŚćńŁŹżę€ąę~&");
+Console.ReadKey();
